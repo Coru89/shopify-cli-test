0e4232fc8178a5413088a614660ae0de63b3b90f
coru89 <corey.james.keller@gmail.com>
2023-02-24 19:33:16 +0700

    more test styles

--- gulpfile.js
+++ gulpfile.js
@@ -3,11 +3,17 @@
 const gulp = require('gulp');
 const sass = require('gulp-sass')(require('node-sass'));
 const autoprefixer = require('gulp-autoprefixer');
+const gulpif = require('gulp-if');
+const rename = require('gulp-rename');
 
 // SASS
 gulp.task('sass', () => {
-    return gulp.src('styles/custom-styles.scss')
-        .pipe(sass())
+    // return gulp.src('styles/custom-styles.scss')
+    return gulp
+        .src('styles/*.scss') // comment out this line if not want to generate individual css files
+        //.src('styles/custom-styles.scss')
+        .pipe(sass().on('error', sass.logError))
+        .pipe(gulpif('*.css', rename({ extname: '.min.css' }))) // comment out this line if not want to generate individual css files
         .pipe(autoprefixer('last 4 version'))
         .pipe(gulp.dest('assets'))
 });
